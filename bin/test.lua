0ba46f23c05df4b666bdd0eee8317978aa9f7fce
Supernicejohn <thesvensk99gmail.com>
2022-09-01 13:10:45 +0200

    Added functionality for program-created extra windows, destruction of windows and p-window ordering is broken for now

---
+++ bin/test.lua
@@ -0,0 +1,10 @@
+local win = {} --sys.requestWindow(10,1,20,20)
+local t = false --win.getTerm()
+--t.write("window!")
+while true do
+    local e = {coroutine.yield()}
+    win[#win+1] = sys.requestWindow(math.random(1,30),math.random(1,30),
+        math.random(20,40),math.random(20,30))
+    t = win[#win].getTerm()
+    t.write(e[1])
+end
